d50a0b3190f8029ba5a6cced7b05765fc9372987
Aditi Bobde <Aditib@WinjitTechnologiesPvtLtd.local>
2020-04-27 21:14:13 +0530

    final static todo

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.get('/todos', function (req, res) {
         filteredTodos = _.where(filteredTodos, {completed: false})
     }else if((queryParams.hasOwnProperty('completed')))
         res.status(400).send();
+
+        if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+           filteredTodos = _.filter(filteredTodos, function(todo){
+               return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
+           })
+        }
     
     res.json(filteredTodos)
 })
